adb84c7144514ee5a6258620d766523a1f7ead6c
Anders Pearson <anders@columbia.edu>
2011-06-06 16:36:33 -0400

    handle anonymous users more gracefully

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="quizblock",
-    version="0.1",
+    version="0.2",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
