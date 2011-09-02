624a732b6b392e171bfefad7fd768e6e235158f6
Anders Pearson <anders@columbia.edu>
2011-09-02 16:33:10 -0400

    make sure explanations get shown when answers are shown

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-quizblock",
-    version="0.4.1",
+    version="0.4.2",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
