1c19f30864d50c1e4a7f8fac9459a19e1cec0610
Anders Pearson <anders@columbia.edu>
2011-11-18 16:07:03 -0500

    breadcrumbs on edit pages. PMT #77530

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-quizblock",
-    version="0.6.3",
+    version="0.6.4",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
