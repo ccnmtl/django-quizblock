3adc0975d6f1e81aec5f83b2ccd85b78670cc48c
Anders Pearson <anders@columbia.edu>
2012-06-26 10:50:36 -0400

    version bump

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup
 
 setup(
     name="django-quizblock",
-    version="0.6.7",
+    version="0.6.8",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
