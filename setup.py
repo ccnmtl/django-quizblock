8e1028ba6beb3f863af37f953d8564eb9a7fb11b
Susan Dreher <sdreher@columbia.edu>
2014-04-19 09:32:47 -0400

    Django 1.6 compatibility: django.conf.urls.defaults removed in favor of django.conf.url

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-quizblock",
-    version="0.6.14",
+    version="0.6.15",
     author="Anders Pearson",
     author_email="anders@columbia.edu",
     url="",
