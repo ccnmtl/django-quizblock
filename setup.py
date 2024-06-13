31e4e9f0d49d8e3de3d3099875b7a3a7f8a0bded
Nikolas Nyby <nikolas@gnu.org>
2024-06-13 14:34:50 -0400

    :bookmark: release version 1.3.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-quizblock",
-    version="1.3.0",
+    version="1.3.1",
     author="Anders Pearson, Evan Petersen",
     author_email="ctl-dev@columbia.edu",
     url="https://github.com/ccnmtl/django-quizblock",
